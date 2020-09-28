9c43df863e6658afada823153fd8ad4c6ac7c2ef
jgregoriods <jonas.gregorio@gmail.com>
2020-09-28 20:08:54 +0200

    add search glyph

--- rongo_prep.py
+++ rongo_prep.py
@@ -1,6 +1,7 @@
 import csv
 import os
 from rongomap import rongomap
+import re
 
 
 tablets = {}
@@ -16,12 +17,15 @@ for csv_file in os.listdir('tablets'):
         side = head[0]
         line = 1
 
+        tablets[tablet][f'{tablet}{side}'] = {}
+
         for row in reader:
-            tablets[tablet][f'{tablet}{side}{line}'] = row[0]
+            tablets[tablet][f'{tablet}{side}'][f'{tablet}{side}{line}'] = row[0]
             line += 1
             if side == head[0] and line > int(head[1]):
                 side = head[2]
                 line = 1
+                tablets[tablet][f'{tablet}{side}'] = {}
 
 
 def clean_data(tablets):
@@ -30,24 +34,27 @@ def clean_data(tablets):
     for tablet in tablets:
         clean_tablets[tablet] = {}
 
-        for line in tablets[tablet]:
-            new_list = []
-            line_list = tablets[tablet][line].replace('.', '-').split('-')
+        for side in tablets[tablet]:
+            clean_tablets[tablet][side] = {}
 
-            for i in line_list:
-                if ':' in i:
-                    a, b = i.split(':')
-                    new_list.append(''.join([c for c in b if c.isdigit()]))
-                    new_list.append(''.join([c for c in a if c.isdigit()]))
+            for line in tablets[tablet][side]:
+                new_list = []
+                line_list = tablets[tablet][side][line].replace('.', '-').split('-')
 
-                else:
-                    new_list.append(''.join([c for c in i if c.isdigit()]))
+                for i in line_list:
+                    if ':' in i:
+                        a, b = i.split(':')
+                        new_list.append(''.join([c for c in b if c.isdigit()]))
+                        new_list.append(''.join([c for c in a if c.isdigit()]))
 
-            for i in range(len(new_list)):
-                for j in range(3 - len(new_list[i])):
-                    new_list[i] = '0' + new_list[i]
+                    else:
+                        new_list.append(''.join([c for c in i if c.isdigit()]))
 
-            clean_tablets[tablet][line] = '-'.join(new_list)
+                for i in range(len(new_list)):
+                    for j in range(3 - len(new_list[i])):
+                        new_list[i] = '0' + new_list[i]
+
+                clean_tablets[tablet][side][line] = '-'.join(new_list)
 
     return clean_tablets
 
@@ -58,18 +65,21 @@ def simplify(tablets):
     for tablet in tablets:
         simple_tablets[tablet] = {}
 
-        for line in tablets[tablet]:
-            new_list = []
-            line_list = tablets[tablet][line].split('-')
+        for side in tablets[tablet]:
+            simple_tablets[tablet][side] = {}
 
-            for i in line_list:
-                if i in rongomap:
-                    new_list.append(rongomap[i])
-                else:
-                    new_list.append(i)
-        
-            simple_tablets[tablet][line] = '-'.join(new_list)
-    
+            for line in tablets[tablet][side]:
+                new_list = []
+                line_list = tablets[tablet][side][line].split('-')
+
+                for i in line_list:
+                    if i in rongomap:
+                        new_list.append(rongomap[i])
+                    else:
+                        new_list.append(i)
+
+                simple_tablets[tablet][side][line] = '-'.join(new_list)
+
     return simple_tablets
 
 
@@ -90,7 +100,7 @@ def get_glyph_counts(tablets):
 
 def get_stats(tablets):
     length, glyphs = get_glyph_counts(tablets)
-    
+
     glyph_list = [g for g in glyphs]
     glyph_list.sort(key=lambda x: glyphs[x], reverse=True)
 
@@ -106,5 +116,36 @@ def get_stats(tablets):
             break
 
 
-get_stats(clean_data(tablets))
-get_stats(simplify(clean_data(tablets)))
+def concordance(tablets):
+    concord = {}
+
+    for tablet in tablets:
+        for side in tablets[tablet]:
+            for line in tablets[tablet][side]:
+                glyphs = tablets[tablet][side][line].split('-')
+
+                for i in range(1, len(glyphs) - 1):
+                    if glyphs[i] not in concord:
+                        concord[glyphs[i]] = {}
+                    a, b = glyphs[i-1], glyphs[i+1]
+                    if (a, b) in concord[glyphs[i]]:
+                        concord[glyphs[i]][(a, b)] += 1
+                    else:
+                        concord[glyphs[i]][(a, b)] = 1
+
+    return concord
+
+
+def search_glyph(glyph, concord):
+    neighbors = [k for k in list(concord[glyph].keys())
+                 if concord[glyph][k] > 1]
+    neighbors.sort(key=lambda x: concord[glyph][x], reverse=True)
+    print('')
+    for n in neighbors:
+        freq = concord[glyph][n]
+        print(f"   {n[0]}  |  {glyph}  |  {n[1]}  ......  x{freq}")
+    print('')
+
+
+c = concordance(clean_data(tablets))
+search_glyph('380', c)
