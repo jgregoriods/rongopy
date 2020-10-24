a8bfe76eab41192898914dc18955a4d3453f33fc
jgregoriods <jonas.gregorio@gmail.com>
2020-10-24 17:00:46 +0200

    add ga

--- ga.py
+++ ga.py
@@ -2,11 +2,11 @@ import keras
 import pickle
 import numpy as np
 
-from random import shuffle
+from random import shuffle, randint
 
 from tablets import tablets_simple
-from rapanui import syllables
-from stats import get_glyph_counts
+from rapanui import corpus, syllables
+from stats import get_glyph_counts, get_syl_counts
 from language_models import vectorizer, preprocess
 
 
@@ -18,9 +18,11 @@ glyphs = list(glyph_dict.keys())
 glyphs.sort(key=lambda x: glyph_dict[x], reverse=True)
 glyphs = glyphs[:50]
 
-shuffle(syllables)
+syl_dict = get_syl_counts(corpus)
+syls = list(syl_dict.keys())
+syls.sort(key=lambda x: syl_dict[x], reverse=True)
 
-key = {glyphs[i]: syllables[i] for i in range(50)}
+key = {glyphs[i]: syls[i] for i in range(len(syls))}
 
 
 def decode_tablets(tablets, key):
@@ -45,3 +47,22 @@ def get_fitness(decoded):
     lstm_score = lstm_probs.mean(axis=0)[0]
 
     return np.mean([svc_score, lstm_score])
+
+
+class GeneticAlgorithm:
+    def __init__(self, pop_size):
+        self.genomes = [syls.copy() for i in range(pop_size)]
+        self.scores = [0 for i in range(pop_size)]
+
+    def mutate(self):
+        for genome in self.genomes:
+            i = randint(0, len(syls) - 2)
+            genome[i], genome[i+1] = genome[i+1], genome[i]
+
+    def get_scores(self):
+        for i in range(len(self.genomes)):
+            genome = self.genomes[i]
+            key = {glyphs[k]: genome[k] for k in range(len(syls))}
+            decoded = decode_tablets(tablets_simple, key)
+            score = get_fitness(decoded)
+            self.scores[i] = score
