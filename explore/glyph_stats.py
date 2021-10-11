9508da9e7d7f271a8e2bd24b90ad2d46ed13292f
jgregoriods <jonas.gregorio@gmail.com>
2021-10-11 20:39:26 +0200

    add matrix to glyphstats

--- explore/glyph_stats.py
+++ explore/glyph_stats.py
@@ -10,8 +10,8 @@ class GlyphStats:
         glyph_counts = {}
         for tablet in self.tablets:
             for line in self.tablets[tablet]:
-                glyphs = self.tablets[tablet][line].split('-')
-                for glyph in glyphs:
+                glyph_list = self.tablets[tablet][line].split('-')
+                for glyph in glyph_list:
                     if glyph != '000':
                         if glyph not in glyph_counts:
                             glyph_counts[glyph] = 0
@@ -31,5 +31,23 @@ class GlyphStats:
         df.columns = ['Glyph', 'Percent', 'Cumulative Percent']
         return df
 
-    def get_matrix(self):
-        pass
+    def get_matrix(self, max_glyphs=50):
+        transition_matrix = np.zeros((max_glyphs+1, max_glyphs+1))
+        glyph_counts = self.get_counts()
+        glyphs = list(glyph_counts.keys())
+        glyphs.sort(key=lambda x: glyph_counts[x], reverse=True)
+        glyphs = glyphs[:max_glyphs]
+        for tablet in self.tablets:
+            for line in self.tablets[tablet]:
+                glyph_list = self.tablets[tablet][line].split('-')
+                for i in range(len(glyph_list) - 1):
+                    glyph = glyph_list[i]
+                    next_glyph = glyph_list[i+1]
+                    j = glyphs.index(glyph) if glyph in glyphs else max_glyphs
+                    k = glyphs.index(next_glyph) if next_glyph in glyphs else max_glyphs
+                    transition_matrix[j, k] += 1
+        row_total = transition_matrix.sum(axis=1)
+        for i in range(len(transition_matrix)):
+            if row_total[i]:
+                transition_matrix[i] /= row_total[i]
+        return transition_matrix
