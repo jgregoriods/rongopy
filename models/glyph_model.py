77f928075fa7bcc0d2703a80b557d22c5dcf7082
jgregoriods <jonas.gregorio@gmail.com>
2021-10-15 12:41:32 +0200

    some fixes to glyph model

--- models/glyph_model.py
+++ models/glyph_model.py
@@ -2,19 +2,20 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import EarlyStopping
 
-from keras.models import Sequential
-from keras.layers import Embedding, LSTM, Dense, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 
 from config import MAX_GLYPHS
 
 
 class GlyphModel:
     def __init__(self, raw_data):
-        self.tokenizer = Tokenizer(num_words=100, oov_token='<OOV>')
+        self.tokenizer = Tokenizer(50, oov_token='<OOV>')
         self.vocab_size = 0
         self.raw_data = raw_data
         self.encoded_data = self.preprocess_data()
@@ -39,7 +40,7 @@ class GlyphModel:
 
         return encoded_data
 
-    def make_training_data(self):
+    def make_training_data(self, test_split):
         sequences = []
         for text in self.encoded_data:
             for i in range(1, MAX_GLYPHS+1):
@@ -49,24 +50,33 @@ class GlyphModel:
         #self.max_sequence_len = max([len(sequence) for sequence in sequences])
 
         sequences = np.array(pad_sequences(sequences, maxlen=MAX_GLYPHS+1, padding='pre'))
+        np.random.shuffle(sequences)
 
         X = sequences[:, :-1]
         y = to_categorical(sequences[:, -1], num_classes=self.vocab_size)
 
-        return X, y
+        train_size = int(len(sequences) * (1 - test_split))
+        X_train = X[:train_size]
+        X_test = X[train_size:]
 
-    def build(self, embed_size, lstm_size):
+        y_train = y[:train_size]
+        y_test = y[train_size:]
+
+        return X_train, y_train, X_test, y_test
+
+    def build(self, embedding_size, lstm_size, dropout):
         self.model = Sequential([
-            Embedding(self.vocab_size, embed_size, input_length=MAX_GLYPHS),
+            Embedding(self.vocab_size, embedding_size, input_length=MAX_GLYPHS),
             LSTM(lstm_size),
-            Dropout(0.5),
+            Dropout(dropout),
             Dense(self.vocab_size, activation='softmax')
         ])
 
         self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    def train(self, X, y, validation_split, epochs):
-        self.history = self.model.fit(X, y, validation_split=validation_split, epochs=epochs, verbose=1)
+    def train(self, X, y, validation_split, batch_size, epochs):
+        es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=20)
+        self.history = self.model.fit(X, y, validation_split=validation_split, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=[es])
         plt.plot(self.history.history['val_accuracy'])
         plt.show()
 
